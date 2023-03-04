65525ebe6962ad859cf9342577c95202990ff4c5
osboxes.org <milktealliance@protonmail.com>
2023-03-04 21:52:53 +0000

    create 4 classes

--- index.js
+++ index.js
@@ -12,4 +12,49 @@ const render = require("./src/page-template.js");
 
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
+const idList = []
+const teamMembers =[]
 
+const appMenu =()=>{
+    function createManager() {
+      console.log("Please build your team members");
+      inquirer.prompt([
+        {
+            type: "input",
+            name: 'managerName',
+            message: "What is the team manager's name?",
+            validate : answer => {
+                if(answer !== ""){
+                    return true
+                }
+                return "Please enter at least one character."
+            }
+
+        },
+        {
+            type: "input",
+            name: "managerId",
+            message: "What is the team manager's id?"
+        },
+        {
+           type: "input",
+           name: "managerEmail",
+           message: "What is the team manager's email?"
+
+        },
+        {
+            type: "input",
+            name: "managerOfficeNumber",
+            message: "What is the team manager's office number?"
+        },
+      ]).then(answers => {
+        const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber);
+        teamMembers.push(manager);
+        idList.push(answers.managerId);
+
+      })
+  }
+  createManager();
+}
+
+appMenu();
